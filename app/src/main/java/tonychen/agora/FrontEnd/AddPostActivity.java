c910435d9637a22b8faaf6d130f80708576e151f
Tony Chen <tonychen0110@gmail.com>
2015-07-21 01:48:04 -0700

    Created add post page

---
+++ app/src/main/java/tonychen/agora/FrontEnd/AddPostActivity.java
@@ -0,0 +1,18 @@
+package tonychen.agora.FrontEnd;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+import tonychen.agora.R;
+
+public class AddPostActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_post);
+
+
+    }
+
+}
